a6d7bc6894ceab76f3c6350b26c0fdcd705b2e7a
Paul Tagliamonte <paultag@ubuntu.com>
2011-10-24 17:33:52 +0000

    adding in copyright stuffs

---
+++ include/conf/copyright.hh
@@ -0,0 +1,9 @@
+#ifndef _CONF_COPYRIGHT_HH_
+#define _CONF_COPYRIGHT_HH_ FOO
+
+#define _AUTHOR   "Paul R. Tagliamonte <tag@pault.ag>"
+#define _LICENSE  "MIT"
+#define _DATE     "2007 - 2011"
+#define _ORG      ""
+
+#endif
